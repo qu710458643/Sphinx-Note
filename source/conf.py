1c67db0f73c2a6f73a444609bda0ab4eb05c3ad3
qu710458643 <xd_jsqu@163.com>
2021-01-10 10:59:43 +0800

    update theme

--- source/conf.py
+++ source/conf.py
@@ -64,7 +64,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
 # Path should be relative to the ``_static`` files directory.
